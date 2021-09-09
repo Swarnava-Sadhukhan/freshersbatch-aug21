36d486559ee3ddae12ea35dc5e077a485208b2d3
parthn316 <89913817+parthn316@users.noreply.github.com>
2021-09-09 18:51:14 +0530

    Add files via upload

---
+++ Java Assgn 1/Armst.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Armst
+{
+	public static void main(String []args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Number");
+		int num=sc.nextInt();
+		int temp=num;
+		int sum=0;
+		while(temp>0)
+		{
+			int dig=temp%10;
+			sum+=(dig*dig*dig);
+			temp/=10;
+		}
+		if(sum==num)
+			System.out.println("Armstrong Number");
+		else
+			System.out.println("Not Armstrong Number");
+	}
+}
---
+++ Java Assgn 1/Tax.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Tax
+{
+	public static void main(String []args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter CTC");
+		double ctc=sc.nextDouble();
+		double tax;
+		if(ctc>0 && ctc<=180000)
+			System.out.println("No Income Tax Applicable");
+		else if(ctc>=181001 && ctc<=300000)
+		{	
+			tax=ctc*0.1;
+			System.out.println("Tax Payable: "+tax);
+		}
+		else if(ctc>=300001 && ctc<=500000)
+		{	
+			tax=ctc*0.2;
+			System.out.println("Tax Payable: "+tax);
+		}
+		else if(ctc>=500001 && ctc<=1000000)
+		{	
+			tax=ctc*0.3;
+			System.out.println("Tax Payable: "+tax);
+		}
+	}
+}
